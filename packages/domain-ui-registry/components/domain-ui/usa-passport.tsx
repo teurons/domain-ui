240042bff990852ce5c0b20edb6ccbce508ec784
Rajiv Seelam <rajiv@betalectic.com>
2025-08-14 14:33:32 +0530

    refactor: update USA passport regex validation rules for accuracy

--- packages/domain-ui-registry/components/domain-ui/usa-passport.tsx
+++ packages/domain-ui-registry/components/domain-ui/usa-passport.tsx
@@ -5,7 +5,7 @@ import type * as React from "react";
 import { RegexInput } from "@workspace/domain-ui-registry/components/domain-ui/regex-input";
 import { Label } from "@workspace/domain-ui-registry/components/ui/label";
 
-const USA_PASSPORT_REGEX = /^[A-Z]?[0-9]{7,9}$/;
+const USA_PASSPORT_REGEX = /^(?:[A-Z][0-9]{8}|[0-9]{9}|[A-Z][0-9]{7})$/;
 
 export function UsaPassport(props: React.ComponentProps<"input">) {
   const generatedId = useId();
